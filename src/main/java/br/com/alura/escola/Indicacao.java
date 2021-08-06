fddfa0f896cda241f0b5b2b65f57a7a0c89b4639
Leandro Neves <leandrosoares6@hotmail.com>
2021-08-06 13:30:57 -0300

    feature: adicao de objeto de valor Indicacao

---
+++ src/main/java/br/com/alura/escola/Indicacao.java
@@ -0,0 +1,15 @@
+package br.com.alura.escola;
+
+public class Indicacao {
+  
+  private Aluno indicado;
+
+  private Aluno indicante;
+
+  public Indicacao(Aluno indicado, Aluno indicante) {
+    this.indicado = indicado;
+    this.indicante = indicante;
+  }
+
+  
+}
