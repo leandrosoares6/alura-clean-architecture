63934b7a7cab17f9512627bf666ffdaa5e487fb5
Leandro Neves <leandrosoares6@hotmail.com>
2021-08-07 06:36:46 -0300

    refactor: criacao de pacote de dominio

--- src/test/java/br/com/alura/escola/dominio/aluno/EmailTest.java
+++ src/test/java/br/com/alura/escola/dominio/aluno/EmailTest.java
@@ -1,11 +1,9 @@
-package br.com.alura.escola;
+package br.com.alura.escola.dominio.aluno;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
-
-import br.com.alura.escola.aluno.Email;
 
 public class EmailTest {
   
