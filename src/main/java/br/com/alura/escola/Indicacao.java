3b4594d984f4e9ba8d8a78fe21f06770846f844d
Leandro Neves <leandrosoares6@hotmail.com>
2021-08-07 06:08:19 -0300

    feature: adicao de factory para criacao de aluno com padrao builder

--- src/main/java/br/com/alura/escola/Indicacao.java
+++ src/main/java/br/com/alura/escola/Indicacao.java
@@ -1,15 +1,31 @@
 package br.com.alura.escola;
 
+import java.time.LocalDateTime;
+
 public class Indicacao {
   
-  private Aluno indicado;
+  private final Aluno indicado;
 
-  private Aluno indicante;
+  private final Aluno indicante;
 
-  public Indicacao(Aluno indicado, Aluno indicante) {
+  private final LocalDateTime dataIndicacao;
+
+  public Indicacao(final Aluno indicado, final Aluno indicante) {
     this.indicado = indicado;
     this.indicante = indicante;
+    this.dataIndicacao = LocalDateTime.now();
   }
 
+  public Aluno getIndicado() {
+    return indicado;
+  }
+
+  public Aluno getIndicante() {
+    return indicante;
+  }
+
+  public LocalDateTime getDataIndicacao() {
+    return dataIndicacao;
+  }
   
 }
